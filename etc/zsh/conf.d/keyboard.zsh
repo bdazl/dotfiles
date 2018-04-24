abbcde84d077627c75583f825e9a33e4b677d4f0
Jacob Peyron <jacob.peyron@gmail.com>
2018-04-24 11:32:17 +0200

    Added moved keyboard setup to ~/.config/zsh
    This commit includes the script code to execute all .zsh files within
    that directory.
    
    Changes committed:
    	new file:   etc/zsh/conf.d/keyboard.zsh
    	modified:   etc/zsh/zshrc
    	modified:   install.conf.yaml

---
+++ etc/zsh/conf.d/keyboard.zsh
@@ -0,0 +1,18 @@
+# Keyboard / ZLE configuration for ZSH
+#
+# Author: Jacob Peyron
+#
+
+
+function config_keybindings_emacs()
+{
+    bindkey -e
+}
+
+function config_keybindings_vim()
+{
+    bindkey -v
+}
+
+config_keybindings_emacs
+#config_keybindings_vim
