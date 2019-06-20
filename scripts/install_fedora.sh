e234e5421b681f39859ba4f128b4fbb62f5f6214
Jacob Peyron <jacob.peyron@gmail.com>
2019-06-20 17:43:26 +0200

    script: install_fedora.sh & install_fedora_gnome3.sh

---
+++ scripts/install_fedora.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+# install essentials
+sudo dnf install \
+    https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+    zsh \
+    tmux \
+	rxvt-unicode \
+	vim \
+    git \
+	python
+
+sudo dnf update
+
+echo Dont forget to generate SSH key
+
+
+# User should have access to:
+#   - terminal -> zsh 
+
+echo Please change default shell. Why does not chsh exist on fedora30?
+#if [ -n "$(which lshch)" ]; then
+#sudo lchsh << EOF
+#$(which zsh)
+#EOF
+#else 
+    #echo Could not install zsh as default shell
+    #echo Trying chsh
+    #chsh -s $(which zsh) $(whoami)
+#fi
+
+
