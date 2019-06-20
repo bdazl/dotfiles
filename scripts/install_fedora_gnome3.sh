acee5146f5f8b92905919e20d31de0a375a08b9e
Jacob Peyron <jacob.peyron@gmail.com>
2019-06-20 18:13:02 +0200

    Fixed bug in echo

--- scripts/install_fedora_gnome3.sh
+++ scripts/install_fedora_gnome3.sh
@@ -3,8 +3,8 @@
 #sudo dnf install \
 
 # set caps -> esc
-echo caps -> esc
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
+echo caps mapped to esc
 
 echo Please set keyboard shortcuts from
 echo Super+enter (Open terminal with tmux urxvt)
