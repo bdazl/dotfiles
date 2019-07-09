6dcd622caee2c5e0903b2b6ad719919247bbb7a1
Jacob Peyron <jacob.peyron@gmail.com>
2019-07-09 14:01:43 +0200

    zsh: cleanup freedesktop env

--- etc/zsh/conf.d/env/freedesktop.zsh
+++ etc/zsh/conf.d/env/freedesktop.zsh
@@ -4,7 +4,7 @@
 #
 # Ensure freedesktop basic variables
 
-[ "$XDG_CONFIG_HOME" ] || export XDG_CONFIG_HOME="$HOME/.config"
-[ "$XDG_DATA_HOME" ] || export XDG_DATA_HOME="$HOME/.local/share"
-[ "$XDG_CACHE_HOME" ] || export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
 
