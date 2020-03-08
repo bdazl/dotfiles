80b6066816a09200a81920ad4419ffce038c7e8b
Jacob Peyron <jacob.peyron@gmail.com>
2020-03-08 10:39:05 +0000

    zsh: added zsh-ssh-agent to antigen

--- etc/zsh/conf.d/0_antigen.zsh
+++ etc/zsh/conf.d/0_antigen.zsh
@@ -11,7 +11,8 @@ source $HOME/.dotfiles/etc/zsh/conf.d/antigen/antigen.zsh
 # Load oh-my-zsh lib
 #antigen use oh-my-zsh
 
-antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle bobsoppe/zsh-ssh-agent
+#antigen bundle zsh-users/zsh-syntax-highlighting
 
 antigen apply
 
