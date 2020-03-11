929dfc94be2fdbffc2436b37949fdc2b17711206
Jacob Peyron <jacob.peyron@gmail.com>
2020-03-11 07:19:07 +0100

    install_pi.sh: add wireguard

--- scripts/install_pi.sh
+++ scripts/install_pi.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-sudo apt-get upgrade
-sudo apt-get install -y git zsh vim vim-nox cmake python3-dev golang
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+apt upgrade
+apt install -y git zsh vim vim-nox cmake python3-dev golang \
+    raspberrypi-kernel-headers libelf-dev libmnl-dev build-essential git \
+    wireguard # might need apt to be in testing
