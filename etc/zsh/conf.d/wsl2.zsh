b99aaf3c4499d08f86d37e6405926b130846c071
Jacob Peyron <jacob.peyron@jeppesen.com>
2022-12-20 15:56:42 +0100

    wsl2: /etc/resolv.conf fixup

---
+++ etc/zsh/conf.d/wsl2.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+separator="#JSEP"
+
+function echo_resolv()
+{
+    echo $separator
+    echo search got.jeppesensystems.com jeppesensystems.com osp.jeppesensystems.com support.jeppesensystems.com testnet.jeppesensystems.com ymq.jeppesensystems.com jeppesen.com corp.gds.jeppesen.com
+    echo nameserver 10.67.6.4
+    echo nameserver 10.67.6.21 
+}
+
+found_sep=$(grep "$separator" /etc/resolv.conf)
+if [ -z "$found_sep" ]; then
+    echo_resolv | sudo tee --append /etc/resolv.conf > /dev/null
+fi
