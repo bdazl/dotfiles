cda1f1d73cd1a689e823b696ead29e76743e5b49
Jacob Peyron <jacob.peyron@gmail.com>
2023-03-19 14:05:24 +0100

    zsh: add wsl script

---
+++ etc/zsh/conf.d/wsl.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+#
+# Author: Jacob Peyron <jacob.peyron@gmail.com>
+#
+# Setup wsl2
+
+is_wsl2()
+{
+    [ -n "$(uname -a | grep WSL2)" ]
+}
+
+start_tmux()
+{
+    [[ $commands[tmux] ]] && [ "$TMUX" = "" ] && tmux
+}
+
+if [ is_wsl2 ]; then
+    start_tmux
+    set bell-style none
+fi
