19efa2c2604a4e309fb86369c6a21889b6dffefb
Jacob Peyron <jacob.peyron@gmail.com>
2023-09-16 11:14:43 +0200

    zsh: create env/session.zsh

---
+++ etc/zsh/conf.d/env/session.zsh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+#
+# Author: Jacob Peyron <jacob.peyron@gmail.com>
+#
+
+if [[ ! $commands[loginctl] ]]; then
+    return
+fi
+
+# Session information
+export SESSION=$(loginctl | grep $USER | awk '{print $1}')
+export SESSION_TYPE=$(loginctl show-session $SESSION | grep Type | tr '=' ' ' | awk '{print $2}')
+
+if [[ "$SESSION_TYPE" == "wayland" ]]; then
+    export WAYLAND="yes"
+fi
+
+# export UID=$(loginctl | grep $USER | awk '{print $2}')
+# export USER=$(loginctl | grep $USER | awk '{print $3}')
+# export USER=$(loginctl | grep $USER | awk '{print $3}')
+export SEAT=$(loginctl | grep $USER | awk '{print $4}')
+# $TTY == /dev/pts/x (or somthn)
+export TTY_=$(loginctl | grep $USER | awk '{print $5}')
