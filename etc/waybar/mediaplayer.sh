c94bdfed5d4801091dfbbb40e5a0cd59b2747518
Jacob Peyron <jacob.peyron@gmail.com>
2023-09-24 14:27:58 +0200

    waybar: changes

--- etc/waybar/mediaplayer.sh
+++ etc/waybar/mediaplayer.sh
@@ -3,5 +3,5 @@ player_status=$(playerctl status 2> /dev/null)
 if [ "$player_status" = "Playing" ]; then
     echo "$(playerctl metadata artist) - $(playerctl metadata title)"
 elif [ "$player_status" = "Paused" ]; then
-    echo " $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo "$(playerctl metadata artist) - $(playerctl metadata title)"
 fi
