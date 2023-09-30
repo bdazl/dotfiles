2f16f410151a2c241616e3a067d3cac50afedbca
Jacob Peyron <jacob.peyron@gmail.com>
2023-09-30 13:52:22 +0200

    zsh: XKB_DEFAULT_OPTIONS=caps:escape (and sway input vars)

--- etc/zsh/conf.d/env/keyboard.zsh
+++ etc/zsh/conf.d/env/keyboard.zsh
@@ -7,9 +7,9 @@
 # Keyboard environment (layout, repeat rate, etc...)
 export XKB_DEFAULT_LAYOUT=se
 export XKB_DEFAULT_MODEL=pc105
-#export XKB_DEFAULT_OPTIONS=caps:none,compose:ralt
+export XKB_DEFAULT_OPTIONS=caps:escape
 #export XKB_DEFAULT_VARIANT=,nodeadkeys
 
-export WLC_REPEAT_DELAY=300
-export WLC_REPEAT_RATE=20
+export WLC_REPEAT_RATE=45
+export WLC_REPEAT_DELAY=250
 
