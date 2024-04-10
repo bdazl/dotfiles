8a4ae6f3f2ddcde7fa6d1809ffd698ee0d53686c
Jacob Peyron <jacob.peyron@gmail.com>
2024-04-10 11:42:32 +0200

    zsh: alias: more changes to time format of ls

--- etc/zsh/conf.d/alias.zsh
+++ etc/zsh/conf.d/alias.zsh
@@ -15,9 +15,9 @@ function config_aliases()
         alias lsblk='sudo diskutil list'
     else
         alias ls='ls --color=auto --group-directories-first'
-        alias la='ls -lha --time-style=iso'
-        alias ll='ls -lh --time-style=iso'
-        alias lll='ls -lhaZ --time-style=full-iso'
+        alias la='ls -lha --time-style=long-iso'
+        alias ll='ls -lh --time-style=+%Y-%m-%d'
+        alias lll='ls -lhaZ --time-style=long-iso'
         alias grep='grep --color=auto'
         alias tl='tree -C | bat'
     fi
