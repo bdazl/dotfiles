ab78a91b96f6d2443b69bd5be492899f2cfb09fe
Jacob Peyron <jacob.peyron@gmail.com>
2024-09-06 08:33:54 +0200

    zsh: completion should not return non-zero exit code

--- etc/zsh/conf.d/completion.zsh
+++ etc/zsh/conf.d/completion.zsh
@@ -5,5 +5,5 @@
 # Author: Jacob Peyron
 #
 
-command -v zoxide &> /dev/null && eval "$(zoxide init --cmd j zsh)"
-command -v gh &> /dev/null && eval "$(gh completion -s zsh)"
+command -v zoxide &> /dev/null && eval "$(zoxide init --cmd j zsh)" || true
+command -v gh &> /dev/null && eval "$(gh completion -s zsh)" || true
