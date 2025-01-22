4509919578877dfc96e35d5c32939131d6b57235
Jacob Peyron <jacob.peyron@gmail.com>
2025-01-22 14:30:05 +0100

    win: profile.ps1

---
+++ etc/powershell/profile.ps1
@@ -0,0 +1,36 @@
+## KEYBINDINGS
+
+# VI-mode keybindings
+Set-PSReadlineOption -EditMode vi
+# TODO: map ö ^
+# TODO: map ö ^
+
+
+# Reload the shell
+Set-Alias reload Reload-Powershell
+
+Set-Alias unzip Expand-Archive
+Set-Alias whereis Get-Command
+
+Set-Alias mv Move-Item
+# Set-Alias cp Copy-Item
+Set-Alias rm Remove-Item
+Set-Alias ls Get-ChildItem
+Set-Alias ll Get-ChildItem
+Set-Alias cat Get-Content
+Set-Alias pwd Get-Location
+# Set-Alias touch New-Item -ItemType File
+# Set-Alias mkdir New-Item -ItemType Directory
+Set-Alias rmdir Remove-Item
+Set-Alias grep Select-String
+Set-Alias df Get-PSDrive
+Set-Alias ps Get-Process
+
+function ga { git add @args }
+function gc { git commit --verbose @args }
+function gd { git diff @args }
+function gs { git status @args }
+function gpp { git pull --prune @args }
+
+# Fonts
+oh-my-posh init pwsh | Invoke-Expression
