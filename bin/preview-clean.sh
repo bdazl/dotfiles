8b248c05d4aeba4291e240d0e5041ab10cbfac62
Jacob Peyron <jacob.peyron@gmail.com>
2025-03-01 15:00:00 +0100

    bin: add redraw logic to preview-clean.sh

--- bin/preview-clean.sh
+++ bin/preview-clean.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+redraw() {
+    # The redraw triggers another clean operation
+    # Add file stuff here to handle re-entrance
+    rfile=/tmp/lf-redraw
+    if [ ! -f $rfile ]; then
+        touch $rfile
+        lf -remote "send redraw"
+    else
+        rm -f $rfile
+    fi
+}
+
 if command -v kitty > /dev/null 2>&1; then
     kitty +kitten icat --clear --stdin no --transfer-mode memory < /dev/null > /dev/tty
+    redraw
 fi
