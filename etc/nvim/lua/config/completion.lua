83a2942d4f7a9712e9737a88123e2a7c9d3dea2d
Jacob Peyron <jacob.peyron@gmail.com>
2026-03-16 14:05:08 +0100

    nvim: don't auto-complete without explicit select

--- etc/nvim/lua/config/completion.lua
+++ etc/nvim/lua/config/completion.lua
@@ -9,7 +9,7 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
         ['<Tab>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
